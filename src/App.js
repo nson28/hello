58b621a71740337779335e7ba725427eb90a5c16
nson28 <nelsontejara@vsu.edu.ph>
2024-07-31 16:14:30 +0800

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const showEmployees =true;
 
   return (
-    <div className="App">
+    <div className="App bg-red-300">
         {showEmployees ? (
         <>
         <input type ='text'onChange={(e)=>{
