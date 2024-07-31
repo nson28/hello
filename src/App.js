b19b192f8960a51844b1282ba5225ed62a6f04d5
nson28 <nelsontejara@vsu.edu.ph>
2024-07-31 11:26:59 +0800

    added props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 import Employee from './components/Employee'
 
 function App() {
+  const showEmployees =true;
+
   return (
     <div className="App">
-      <header className="App-header">
+        {showEmployees ? (
+        <>
+          <Employee name = "Nelson"  role="Intern"/>
+          <Employee name = "Rio"/>
+          <Employee name = "Irene"/>
+          <Employee />
+          <Employee />
+        </>                
         
-         <Employee />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ): (
+          <p>you cannot see the employees</p>
+      )
+
+      } 
+      
     </div>
   );
 }
